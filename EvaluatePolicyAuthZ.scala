afa2c00b2ade318e634a82abc1614c4fb5cd06c2
James Billingham <msejdb@yahoo.co.uk>
2019-07-09 12:21:43 +0100

    working

--- EvaluatePolicyAuthZ.scala
+++ EvaluatePolicyAuthZ.scala
@@ -3,11 +3,13 @@ package openam.authntree
 import io.gatling.core.Predef._
 import scala.concurrent.duration._
 class getEvaluatePolicy extends AuthNTreeSimulation {
+  val admin_id = System.getProperty("admin_id", "amadmin")
+  val admin_pwd = System.getProperty("admin_pwd", "password")
 
   val scn = scenario("evaluatePolicy")
     .during(duration) {
       feed(userFeeder)
-        .exec(usernamePasswordRestLoginPageNode("amadmin", "Intu1tP0C$&@", realm))
+        .exec(usernamePasswordRestLoginPageNode(admin_id, admin_pwd, realm))
         .repeat(100) {
           exec(evaluatePolicy("https://login.test.kubernetes.org.uk/web/sensitive/","${tokenId}"))
           .pause(pause seconds)
