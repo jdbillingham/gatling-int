140b1ae90ae6e0b55025e15ec8a64369cba3593f
James Billingham <msejdb@yahoo.co.uk>
2019-07-09 18:02:52 +0100

    final

--- EvaluatePolicyAsUser.scala
+++ EvaluatePolicyAsUser.scala
@@ -2,15 +2,19 @@ package openam.authntree
 
 import io.gatling.core.Predef._
 import scala.concurrent.duration._
-class getEvaluatePolicy extends AuthNTreeSimulation {
+class getEvaluatePolicyAsUser extends AuthNTreeSimulation {
   val admin_id = System.getProperty("admin_id", "amadmin")
   val admin_pwd = System.getProperty("admin_pwd", "password")
 
   val scn = scenario("evaluatePolicy")
-    .exec(usernamePasswordRestLoginPageNode(admin_id, admin_pwd, realm))
-    .during(duration) {
-          exec(evaluatePolicy("https://login.test.kubernetes.org.uk/web/sensitive/","${tokenId}"))
-          .pause(pause seconds)
-        }
+  .during(duration) {
+    feed(userFeeder)
+      .exec(usernamePasswordRestLoginPageNode("${username}", "${password}", realm))
+      .repeat(100) {
+         exec(evaluatePolicy("https://login.test.kubernetes.org.uk/web/sensitive/","${tokenId}"))
+        .pause(pause seconds)
+      }
+  }
   setUp(scn.inject(rampUsers(concurrency) during (warmup seconds))).protocols(httpProtocol).exponentialPauses
+
 }
